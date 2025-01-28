d4f2eeb55f585244eabb0d8bf9716b7157fa4cf6
Dmitriy Badenskiy <dima.badenskiy96@mail.ru>
2025-01-28 20:51:15 +0300

    TestingTheActionsCommandOnADropBox

---
+++ src/test/java/TestDragDrop.java
@@ -0,0 +1,38 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.DragAndDropOptions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TestDragDrop {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.browserSize = "1474x822";
+        Configuration.timeout = 5000;
+        Configuration.holdBrowserOpen = true;
+    }
+    @Test
+    void TestMovingTheDropBox() {
+        open ("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
+        $("#column-a").dragAndDrop(DragAndDropOptions.to("#column-b"));
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+
+    }
+    @Test
+    void TestingTheActionsCommandOnADropBox() {
+        open ("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
+        actions().clickAndHold($("#column-a")).moveToElement($("#column-b")).release().perform();
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+}
